3b7e244ed0d193e76f50e3bcf37f18d640860da9
jumbo vénommarri <63607964+raphy84@users.noreply.github.com>
2022-01-13 13:14:28 +0000

    solution

--- covid19v3.py
+++ covid19v3.py
@@ -16,10 +16,10 @@ from covid19v2 import *
 
 def data_for_country(data, state = "", country = ""):
     # for each country in data
-    for pays in ___:
+    for pays in data:
         # if 'Province/State' key match with state given in argument
         # and if 'Country/Region' key match with country given in argument
-        if pays[___] == ___ and pays[___] == ___:
+        if pays["Province/State"] == state and pays["Country/Region"] == country:
             return pays
 
 
@@ -39,6 +39,12 @@ if __name__ == '__main__':
       # for row in coutries:
       #    print(row['Province/State'], row['Country/Region'])
       france = data_for_country(countries, '', 'France')
+      print(france)
+
+      print('')
+      print('')
+
+      italie = data_for_country(countries, '', 'Italy')
       print(france)
 
   else:
